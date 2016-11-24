837ab91ce273b2f51363b02c6ed82d116bb2e9e9
kristi <kristi.stanescu@gmail.com>
2016-11-24 10:55:25 +0000

    externalized markup converter

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var Markdown = require('markdown-to-html').Markdown;
 var md = new Markdown();
 md.bufmax = 2048;
-var fileName = '../../WhatToCook/README.md';
+var fileName = '../WhatToCook/README.md';
 var fs = require('fs');
 var fsE = require('fs-extra');
 
-var opts = {title: 'File $BASENAME in $DIRNAME'};
 
-// Write a header.
+// What to cook
+var opts = {title: 'File $BASENAME in $DIRNAME'};
+var dir = "WhatToCook/"
 console.log('===============================');
-// Write a trailer at eof.
 md.once('end', function() {
   console.log('===============================');
 });
@@ -18,20 +18,23 @@ md.render(fileName, opts, function(err) {
     console.error('>>>' + err);
     process.exit();
   }
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
   //var html = md.pipe(process.stdout);
-  var stream = fs.createWriteStream("index.html");
+  var stream = fs.createWriteStream(dir+"index.html");
   stream.once('open', function(fd) {
     stream.write(md.html);
     stream.end();
   });
 
-  fsE.copy('../../WhatToCook/readmeImages', 'readmeImages', function (err) {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log("success!");
-  }
-}); //copies directory, even if it has subdirectories or files
+  fsE.copy('../WhatToCook/readmeImages', dir+'readmeImages', function (err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("success!");
+    }
+  }); //copies directory, even if it has subdirectories or files
 
   console.log("content exported to index.html")
   console.log('===============================');
