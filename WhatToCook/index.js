24e4da2ad610bcd202fa0ba6919f90b0b5cec182
kristi <kristi.stanescu@gmail.com>
2016-11-24 10:47:42 +0000

    npm start now automatically generates this page from ../../README.md

--- WhatToCook/index.js
+++ WhatToCook/index.js
@@ -2,6 +2,9 @@ var Markdown = require('markdown-to-html').Markdown;
 var md = new Markdown();
 md.bufmax = 2048;
 var fileName = '../../WhatToCook/README.md';
+var fs = require('fs');
+var fsE = require('fs-extra');
+
 var opts = {title: 'File $BASENAME in $DIRNAME'};
 
 // Write a header.
@@ -15,5 +18,21 @@ md.render(fileName, opts, function(err) {
     console.error('>>>' + err);
     process.exit();
   }
-  md.pipe(process.stdout);
+  //var html = md.pipe(process.stdout);
+  var stream = fs.createWriteStream("index.html");
+  stream.once('open', function(fd) {
+    stream.write(md.html);
+    stream.end();
+  });
+
+  fsE.copy('../../WhatToCook/readmeImages', 'readmeImages', function (err) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("success!");
+  }
+}); //copies directory, even if it has subdirectories or files
+
+  console.log("content exported to index.html")
+  console.log('===============================');
 });
