31382ebf93841d4b190df7a233d66c58efc5dd7e
kristi <kristi.stanescu@gmail.com>
2018-10-17 22:13:15 +0300

    adde cookie challenges

---
+++ cookie-challenges/precache-manifest.48166cca63818b81b0b02700ac00afb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fcb1f38c3498ea42cd85",
+    "url": "/static/js/main.fcb1f38c.chunk.js"
+  },
+  {
+    "revision": "3c8b542fccae87122b0c",
+    "url": "/static/js/1.3c8b542f.chunk.js"
+  },
+  {
+    "revision": "fcb1f38c3498ea42cd85",
+    "url": "/static/css/main.22c997d5.chunk.css"
+  },
+  {
+    "revision": "ad219b5264219bd21cab76b867a6bf85",
+    "url": "/index.html"
+  }
+];
